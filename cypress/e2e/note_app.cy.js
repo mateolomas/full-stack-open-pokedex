4afc142233ddf18735ede91a3c91109a2ce130e0
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-07-19 15:41:54 -0500

    8080

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -4,7 +4,7 @@ describe('Pokedex', function () {
   it('front page can be opened', function () {
 
 
-    cy.visit('http://localhost:8081')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
 
 
